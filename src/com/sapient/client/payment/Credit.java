34f0853b8f9d825ab00ef13a439b187efc3c409d
Shivam Prakash <sp160194@gmail.com>
2015-09-03 22:48:45 +0530

    comments added incredit.java class

--- src/com/sapient/client/payment/Credit.java
+++ src/com/sapient/client/payment/Credit.java
@@ -3,6 +3,11 @@ package com.sapient.client.payment;
 import java.util.Calendar;
 import java.util.Date;
 
+/**
+ * 
+ * @author Shivam
+ *
+ */
 public class Credit extends Payment implements Authorization {
 
 	private int number;
@@ -11,33 +16,50 @@ public class Credit extends Payment implements Authorization {
 	
 	
 	
-
-
+	/**
+	 * This methods gets credit card number.
+	 * @return
+	*/
 	public int getNumber() {
 		return number;
 	}
 
-
+	/**
+	 * This methods sets credit card number.
+	 * @param number
+	*/
 	public void setNumber(int number) {
 		this.number = number;
 	}
 
-
+	/**
+	 * This methods gets credit card Type.
+	 * @return
+	 */
 	public String getType() {
 		return type;
 	}
 
-
+	/**
+	 * This methods sets credit card Type.
+	 * @param type
+	*/
 	public void setType(String type) {
 		this.type = type;
 	}
 
-
+	/**
+	 * This methods gets credit card ecpire date.
+	 * @return
+	 */
 	public Date getDate() {
 		return date;
 	}
 
-
+	/**
+	 * This methods sets credit card expire date.
+	 * @param date
+	*/
 	public void setDate(Date date) {
 		this.date = date;
 	}
@@ -48,32 +70,41 @@ public class Credit extends Payment implements Authorization {
 	@SuppressWarnings("deprecation")
 	@Override
 	public boolean authorized() {
-		// TODO Auto-generated method stub
 		Integer number=  this.number;
 		String numberInString = number.toString();
 		int length = numberInString.length();
 		
+		/**
+		 *  This is to check credit card should contain exactly 8 digits.
+		 *   I assumed this norm from my side
+		 *   It can be replaced with real norms if required.
+		 *  
+		 */
 		if(length != 8){
 			return false;
 		}
 		
-		
+		/**
+		 * This is to check credit card number should not start with zero.
+		 */
 		String notStartCharacter= "0";
 		if(numberInString.startsWith(notStartCharacter)){
 			return false;	
 		}
-		
-		Calendar calender =Calendar.getInstance();
-		int year = calender.get(Calendar.YEAR);
-		int month = calender.get(Calendar.MONTH);
-		int date = calender.get(Calendar.DATE);
-		Date currentDate = new Date(year, month, date);
+		/**
+		 * This is to check credit card expire date is later than current date.
+		 */
+		Calendar calender =Calendar.getInstance();//New instance of calendar class.
+		int year = calender.get(Calendar.YEAR);// Fetches current year.
+		int month = calender.get(Calendar.MONTH);// Fetches current month.
+		int date = calender.get(Calendar.DATE);// Fetches current date.
+		Date currentDate = new Date(year, month, date);// New date object with  current date.
 		
 		if( currentDate.before(this.date)){
 			return false;
 		}
 		
-		return true;
+		return true;//returns true as not condition has turned false.
 	}
 	
 
